42fa4072bc61c2464b65b0c6d8f8882a652610cd
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-08-02 09:22:42 +0530

    added three origin

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.use(
-  cors({
-    // origin: "https://mern-note1-app.vercel.app",
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  "https://mern-note1-app.vercel.app",
+  "http://localhost:5173",
+  "https://mern-note-app1.netlify.app"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+
+
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGO_URI;
 
